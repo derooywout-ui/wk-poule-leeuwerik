efcdcdbbc85944176858b95b65d6758751408115
derooywout-ui <derooywout@gmail.com>
2026-06-13 17:55:35 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,4 @@
-"use client"
-import { useEffect, useState } from "react"
 import App from "@/components/wk-poule-app"
-
 export default function Page() {
-  const [mounted, setMounted] = useState(false)
-  useEffect(() => { setMounted(true) }, [])
-  if (!mounted) return null
   return <App />
 }

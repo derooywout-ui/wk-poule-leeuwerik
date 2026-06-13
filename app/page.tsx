4f343d30b7b53bb9210dac266bc5977d0abaf243
derooywout-ui <derooywout@gmail.com>
2026-06-13 17:37:48 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
-
-import App from "@/components/wk-poule-app"
-
+import dynamic from "next/dynamic"
+const App = dynamic(() => import("@/components/wk-poule-app"), { ssr: false })
 export default function Page() {
   return <App />
 }
